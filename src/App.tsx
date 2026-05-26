fe038e2e2fd78b5da9e38e22673a4be6fa42df6c
thxrstx <thirsty.dev@proton.me>
2026-05-26 11:25:52 -0500

    chore: proyecto base con React, Vite y Tailwind

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+        <h1 className="text-3xl font-bold text-gray-800">ObraAdmin</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
